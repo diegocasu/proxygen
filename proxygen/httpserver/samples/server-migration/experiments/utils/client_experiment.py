4d62fbda37fbd8dfe4e63eb0bffc73e6f1dd095f
Diego Casu <d.casu3@studenti.unipi.it>
2022-07-12 10:31:06 +0200

    Add support for the second experiment in the scripts

--- proxygen/httpserver/samples/server-migration/experiments/utils/client_experiment.py
+++ proxygen/httpserver/samples/server-migration/experiments/utils/client_experiment.py
@@ -2,6 +2,8 @@ import json
 import sys
 import pandas as pd
 
+from utils.migrate_server_source import MigrationTechnique
+
 
 class ClientExperimentManager:
     def __init__(self, experiment_id):
@@ -19,6 +21,8 @@ class ClientExperimentManager:
 
         if self._id == 1:
             self._initialize_first_experiment()
+        elif self._id == 2:
+            self._initialize_second_experiment()
 
         # Check that the given experiment ID and
         # the one of the configuration match.
@@ -43,8 +47,26 @@ class ClientExperimentManager:
              "symmetric"]
 
         self._results = {"experiment": [], "run": [], "repetition": [],
-                         "seed": [], "serviceTimes [us]": []}
+                         "seed": [], "serviceTimes [us]": [],
+                         "serverAddresses": []}
         self._results_file = "experiment1_service_times.csv"
+
+    def _initialize_second_experiment(self):
+        self._parse_base_config("experiment2_client.json")
+        self._migration_protocols_sequence = \
+            ["proactiveExplicit",
+             "reactiveExplicit",
+             "poolOfAddresses",  # Pool size equal to 3
+             "symmetric"]
+
+        # Use the same protocol with all the migration techniques.
+        self._n_migration_techniques = len(MigrationTechnique)
+        self._current_migration_technique = 0
+
+        self._results = {"experiment": [], "run": [], "repetition": [],
+                         "seed": [], "serviceTimes [us]": [],
+                         "serverAddresses": []}
+        self._results_file = "experiment2_service_times.csv"
 
     def _get_new_config_first_experiment(self):
         if self._current_repetition >= self._n_repetitions_per_run \
@@ -68,9 +90,41 @@ class ClientExperimentManager:
         self._current_config["seed"] = self._current_seed
         return self._current_config
 
+    def _get_new_config_second_experiment(self):
+        if self._current_repetition >= self._n_repetitions_per_run \
+                or self._current_run == 0:
+            if self._current_run == 0:
+                self._current_config["experiment"]["serverMigrationProtocol"] = \
+                    self._migration_protocols_sequence.pop(0)
+
+            self._current_run += 1
+            self._current_repetition = 1
+            self._current_seed += 1
+            self._current_migration_technique += 1
+
+            if self._current_migration_technique > self._n_migration_techniques:
+                if not self._migration_protocols_sequence:
+                    # All the configurations have been crafted,
+                    # so end the experiment.
+                    return None
+
+                self._current_migration_technique = 1
+                self._current_config["experiment"]["serverMigrationProtocol"] = \
+                    self._migration_protocols_sequence.pop(0)
+
+            self._current_config["seed"] = self._current_seed
+            return self._current_config
+
+        self._current_repetition += 1
+        self._current_seed += 1
+        self._current_config["seed"] = self._current_seed
+        return self._current_config
+
     def get_new_config(self):
         if self._id == 1:
             return self._get_new_config_first_experiment()
+        elif self._id == 2:
+            return self._get_new_config_second_experiment()
 
         return None
 
@@ -80,10 +134,23 @@ class ClientExperimentManager:
         self._results["repetition"].append(self._current_repetition)
         self._results["seed"].append(self._current_seed)
         self._results["serviceTimes [us]"].append(service_times["serviceTimes"])
+        self._results["serverAddresses"] \
+            .append(service_times["serverAddresses"])
+
+    def _save_service_times_second_experiment(self, service_times):
+        self._results["experiment"].append(self._id)
+        self._results["run"].append(self._current_run)
+        self._results["repetition"].append(self._current_repetition)
+        self._results["seed"].append(self._current_seed)
+        self._results["serviceTimes [us]"].append(service_times["serviceTimes"])
+        self._results["serverAddresses"] \
+            .append(service_times["serverAddresses"])
 
     def save_service_times(self, service_times):
         if self._id == 1:
             self._save_service_times_first_experiment(service_times)
+        elif self._id == 2:
+            self._save_service_times_second_experiment(service_times)
 
     def dump_experiment_results_to_file(self):
         df = pd.DataFrame(self._results)
--- proxygen/httpserver/samples/server-migration/experiments/utils/server_experiment.py
+++ proxygen/httpserver/samples/server-migration/experiments/utils/server_experiment.py
@@ -28,6 +28,14 @@ class ServerExperimentManager:
                 print("Wrong base configuration for address pool")
                 sys.exit(1)
 
+        elif self._id == 2:
+            self._initialize_second_experiment()
+
+            # Consistency check.
+            if len(self._current_config["serverMigration"]["addressPool"]) != 3:
+                print("Wrong base configuration for address pool. "
+                      "It must be composed of 3 addresses")
+
     def _parse_base_config(self, config_name):
         config_path = "./base_configs/" + config_name
         with open(config_path, "r") as base_config_file:
@@ -61,6 +69,25 @@ class ServerExperimentManager:
                          "dumpTime [s]": [], "dumpTxTime [s]": [],
                          "dumpSize": [], "preDumpSize": []}
         self._results_file = "experiment1_migration_times.csv"
+
+    def _initialize_second_experiment(self):
+        self._parse_base_config("experiment2_server_source.json")
+        self._migration_protocols_sequence = \
+            ["proactiveExplicit",
+             "reactiveExplicit",
+             "poolOfAddresses3",  # Pool size equal to 3
+             "symmetric"]
+        self._current_migration_protocol = None
+
+        self._migration_techniques = [e for e in MigrationTechnique]
+        self._current_migration_technique = None
+
+        self._results = {"experiment": [], "run": [], "repetition": [],
+                         "seed": [], "migrationTechnique": [], "protocol": [],
+                         "preDumpTime [s]": [], "preDumpTxTime [s]": [],
+                         "dumpTime [s]": [], "dumpTxTime [s]": [],
+                         "dumpSize": [], "preDumpSize": []}
+        self._results_file = "experiment2_migration_times.csv"
 
     def _get_new_config_first_experiment(self):
         if self._current_repetition >= self._n_repetitions_per_run \
@@ -107,9 +134,59 @@ class ServerExperimentManager:
         self._current_config["seed"] = self._current_seed
         return self._current_config, self._migration_technique
 
+    def _get_new_config_second_experiment(self):
+        if self._current_repetition >= self._n_repetitions_per_run \
+                or self._current_run == 0:
+            self._current_run += 1
+            self._current_repetition = 1
+            self._current_seed += 1
+
+            self._current_config["seed"] = self._current_seed
+
+            if not self._migration_techniques or self._current_run == 1:
+                if not self._migration_protocols_sequence:
+                    # All the configurations have been crafted,
+                    # so end the experiment.
+                    return None, None
+
+                self._migration_techniques = [e for e in MigrationTechnique]
+                self._current_migration_technique = \
+                    self._migration_techniques.pop(0)
+
+                migration_protocols = self._current_config["serverMigration"]
+                self._current_migration_protocol = \
+                    self._migration_protocols_sequence.pop(0)
+
+                next_protocol = self._current_migration_protocol
+                if "Explicit" in next_protocol:
+                    next_protocol = "explicit"
+                elif "poolOfAddresses" in next_protocol:
+                    next_protocol = "poolOfAddresses"
+
+                # Enable only the migration protocol required by the run.
+                for protocol in migration_protocols:
+                    if protocol == "enable" or protocol == "addressPool":
+                        continue
+                    if protocol == next_protocol:
+                        migration_protocols[protocol] = True
+                    else:
+                        migration_protocols[protocol] = False
+            else:
+                self._current_migration_technique = \
+                    self._migration_techniques.pop(0)
+
+            return self._current_config, self._current_migration_technique
+
+        self._current_repetition += 1
+        self._current_seed += 1
+        self._current_config["seed"] = self._current_seed
+        return self._current_config, self._current_migration_technique
+
     def get_new_config(self):
         if self._id == 1:
             return self._get_new_config_first_experiment()
+        elif self._id == 2:
+            return self._get_new_config_second_experiment()
 
         return None, None
 
@@ -129,9 +206,27 @@ class ServerExperimentManager:
         self._results["dumpSize"].append(migration_times["dumpSize"])
         self._results["preDumpSize"].append(migration_times["preDumpSize"])
 
+    def _save_migration_measurements_second_experiment(self, migration_times):
+        self._results["experiment"].append(self._id)
+        self._results["run"].append(self._current_run)
+        self._results["repetition"].append(self._current_repetition)
+        self._results["seed"].append(self._current_seed)
+        self._results["migrationTechnique"] \
+            .append(str(self._migration_technique.name).lower())
+        self._results["protocol"].append(self._current_migration_protocol)
+        self._results["preDumpTime [s]"].append(migration_times["preDumpTime"])
+        self._results["preDumpTxTime [s]"] \
+            .append(migration_times["preDumpTxTime"])
+        self._results["dumpTime [s]"].append(migration_times["dumpTime"])
+        self._results["dumpTxTime [s]"].append(migration_times["dumpTxTime"])
+        self._results["dumpSize"].append(migration_times["dumpSize"])
+        self._results["preDumpSize"].append(migration_times["preDumpSize"])
+
     def save_migration_measurements(self, migration_times):
         if self._id == 1:
             self._save_migration_measurements_first_experiment(migration_times)
+        elif self._id == 2:
+            self._save_migration_measurements_second_experiment(migration_times)
 
     def dump_experiment_results_to_file(self):
         df = pd.DataFrame(self._results)
