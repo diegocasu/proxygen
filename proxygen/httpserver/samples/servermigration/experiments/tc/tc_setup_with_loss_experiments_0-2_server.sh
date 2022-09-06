06538c2ebfc05c0ba7c9b6fbacf1a30f6bbbef85
Diego Casu <d.casu3@studenti.unipi.it>
2022-09-06 19:26:37 +0200

    Update tc scripts

---
+++ proxygen/httpserver/samples/servermigration/experiments/tc/tc_setup_with_loss_experiments_0-2_server.sh
@@ -0,0 +1,6 @@
+sudo tc qdisc del dev enp4s0 root
+sudo tc qdisc add dev enp4s0 root handle 1: htb
+
+sudo tc class add dev enp4s0 parent 1: classid 1:1 htb rate 100mbit
+sudo tc filter add dev enp4s0 protocol ip parent 1:0 prio 1 u32 match ip dst 192.168.1.57 match ip sport 6666 0xffff flowid 1:1
+sudo tc qdisc add dev enp4s0 parent 1:1 handle 10: netem loss 1.5%
